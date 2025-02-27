5d247ecb6c3a1571e5c8d333e21e48922baea136
ERICA VIANA SOARES <erica.vsoares@senacsp.edu.br>
2025-02-27 20:46:46 -0300

    Atualização

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
 console.log("Electron - Processo principal")
 
+// Importação dos recursos do framework
+// app -> aplicação
+// BrowserWindow -> criação da janela
 const { app, BrowserWindow } = require('electron/main')
 
+// Janela principal
+let win
 const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600
+  win = new BrowserWindow({
+    width: 1010, // Largura
+    height: 720, // Altura
+    // frame: false, // Tela para totem de autoatendimento
+    // resizable: false, // Maximizar
+    // minimizable: false, // Minimizar
+    // closable: false, // Fechar
+    // autoHideMenuBar: true // Esconder o menu do browser
   })
 
+  // Carregar o documento HTML na janela
   win.loadFile('./src/views/index.html')
 }
 
+// Inicialização da aplicação (assincronismo)
 app.whenReady().then(() => {
   createWindow()
 
+  // Só ativar a janela principal se nenhuma outra estiver ativa
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow()
@@ -21,6 +34,7 @@ app.whenReady().then(() => {
   })
 })
 
+// Se o sistema não for MAC, encerrar a aplicação quando a janela for fechada
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
