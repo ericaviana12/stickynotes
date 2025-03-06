4045e590fcf9af04f2363519aa9dbd88df0f6aaa
ERICA VIANA SOARES <erica.vsoares@senacsp.edu.br>
2025-03-06 20:59:21 -0300

    Atualização

--- main.js
+++ main.js
@@ -3,14 +3,16 @@ console.log("Electron - Processo principal")
 // Importação dos recursos do framework
 // app -> aplicação
 // BrowserWindow -> criação da janela
-// nativeTheme está relacionado ao tema claro ou escuro (definir)
-const { app, BrowserWindow, nativeTheme} = require('electron/main')
+// nativeTheme -> definir o tema claro, escuro ou padrão do sistema
+// Menu -> definir um menu personalizado
+// Shell -> Acessar links externos no navegador padrão
+const { app, BrowserWindow, nativeTheme, Menu, shell } = require('electron/main')
 
 // Janela principal
 let win
 const createWindow = () => {
   // definindo tema da janela claro ou escuro
-  nativeTheme.themeSource = 'dark'
+  nativeTheme.themeSource = 'light'
   win = new BrowserWindow({
     width: 1010, // Largura
     height: 720, // Altura
@@ -21,8 +23,35 @@ const createWindow = () => {
     // autoHideMenuBar: true // Esconder o menu do browser
   })
 
+  // Carregar o menu personalizado
+  // Atenção! Antes importar o recurso Menu
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+
+
   // Carregar o documento HTML na janela
   win.loadFile('./src/views/index.html')
+}
+
+// Janela sobre
+let about
+function aboutWindow() {
+  nativeTheme.themeSource='light'
+  // Obter a janela principal
+  const mainWindow = BrowserWindow.getFocusedWindow()
+  // Validação (se existir a janela principal)
+  if (mainWindow) {
+    about = new BrowserWindow({
+    width: 320, // Largura
+    height: 280, // Altura
+    autoHideMenuBar: true, // Esconder o menu do browser
+    resizable: false, // Maximizar
+    minimizable: false, // Minimizar
+    parent: mainWindow, // Estabelecer uma relação hierárquica entre janelas
+    modal: true // Criar uma janela modal
+  })
+  }
+  
+  about.loadFile('./src/views/sobre.html')
 }
 
 // Inicialização da aplicação (assincronismo)
@@ -43,3 +72,67 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
+
+// Reduzir a verbosidade de logs não críticos (devtools)
+app.commandLine.appendSwitch('log-level', '3')
+
+// Template do menu
+// Abertura e fechamento em [] é para a criação de um vetor
+// Obs.: Abertura e fechamento em {} é para a criação de um objeto
+const template = [
+  {
+    label: 'Notas',
+    submenu: [
+      {
+        label: 'Criar nota',
+        accelerator: 'Ctrl+N',
+        // click: () => console.log("Teste")
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Sair',
+        accelerator: 'Alt+F4',
+        click: () => app.quit()
+      }
+    ]
+  },
+  {
+    label: 'Ferramentas',
+    submenu: [
+      {
+        label: 'Aplicar zoom',
+        role: 'zoomIn'
+      },
+      {
+        label: 'Reduzir zoom',
+        role: 'zoomOut'
+      },
+      {
+        label: 'Restaurar zoom padrão',
+        role: 'resetZoom'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'DevTools',
+        role: 'toggleDevTools'
+      }
+    ]
+  },
+  {
+    label: 'Ajuda',
+    submenu: [
+      {
+        label: 'Repositório',
+        click: () => shell.openExternal('https://github.com/ericaviana12/stickynotes')
+      },
+      {
+        label: 'Sobre',
+        click: () => aboutWindow()
+      }
+    ]
+  }
+]
