8577c1a994d8852272b6be1ff1c0e4c08c4818c4
ERICA VIANA SOARES <erica.vsoares@senacsp.edu.br>
2025-03-18 20:45:54 -0300

    Atualização

--- preload.js
+++ preload.js
@@ -1,8 +1,18 @@
+/**
+ * preload.js -> Usado no framework electron para aumentar a segurança e o desempenho
+ */
+
 // Importação dos recursos do framework electron
 // ipcRenderer permite estabelecer uma comunicação entre processos (IPC) main.js <=> renderer.js
-const {ipcRenderer} = require('electron')
+// contextBridge: permissões de comunicação entre processos usando a API do electron
+const {ipcRenderer, contextBridge} = require('electron')
 
 // Enviar uma mensagem para o main.js e estabelecer uma conexão com o banco de dados quando iniciar a aplicação
 // send (enviar)
 //db-connect (rótulo para identificar a mensagem)
 ipcRenderer.send('db-connect')
+
+// Permissões para estabelecer a comunicação entre processos
+contextBridge.exposeInMainWorld('api', {
+
+})
